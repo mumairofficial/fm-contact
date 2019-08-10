b7c4b8db4117873823738685748aaffcc5db2a65
Muhammad Umair <mumairofficial@gmail.com>
2019-08-10 17:47:25 +0500

    perf: core module introduction
    Core module for managing application level core services and components

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { ComponentsModule } from "./components/components.module";
+
+@NgModule({
+  exports: [ComponentsModule]
+})
+export class CoreModule {}
