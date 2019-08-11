fdf4040645f03c3cb2085f7d5eb167ee81061e0b
Muhammad Umair <mumairofficial@gmail.com>
2019-08-11 20:06:56 +0500

    feat: added logic to save contact using event emitter

--- src/app/pages/contacts-home/components/contact-form/contact-form.component.ts
+++ src/app/pages/contacts-home/components/contact-form/contact-form.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { IContactItem } from "src/app/shared/models/contact-item.interface";
 
 @Component({
   selector: "contact-form",
-  templateUrl: "contact-form.component.html"
+  templateUrl: "contact-form.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactFormComponent implements OnInit {
   public contactForm: FormGroup;
+
+  @Output() save: EventEmitter<IContactItem> = new EventEmitter<IContactItem>();
 
   constructor(private fb: FormBuilder) {}
 
@@ -16,10 +20,19 @@ export class ContactFormComponent implements OnInit {
 
   private initiateContactForm(): void {
     this.contactForm = this.fb.group({
-      id: [new Date().getTime()],
       fullName: ["", Validators.required],
       email: ["", Validators.compose([Validators.required, Validators.email])],
       phone: [""]
     });
+  }
+
+  onSaveContact(): void {
+    if (this.contactForm.invalid) {
+      alert("Invalid Form");
+      return;
+    }
+    const formValue: IContactItem = { ...this.contactForm.value, id: new Date().getTime() };
+    this.save.emit(formValue);
+    this.contactForm.reset();
   }
 }
