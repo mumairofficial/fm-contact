a68796921eed4f3fb4f610d25ad0d0c6576c7899
Muhammad Umair <mumairofficial@gmail.com>
2019-08-11 20:08:36 +0500

    feat: observable contact list logic and event hook for create contact

--- src/app/pages/contacts-home/contacts-home.component.ts
+++ src/app/pages/contacts-home/contacts-home.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { IContactItem } from "src/app/shared/models/contact-item.interface";
 import { ContactService } from "src/app/core/contact.service";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "contacts-home-page",
   templateUrl: "contacts-home.component.html"
 })
 export class ContactsHomeComponent implements OnInit {
-  public contacts: Array<IContactItem> = [];
+  public contacts$: BehaviorSubject<Array<IContactItem>>;
 
   constructor(private contactsService: ContactService) {}
 
   ngOnInit() {
-    this.contacts = this.contactsService.contacts;
+    this.contacts$ = this.contactsService.contacts$;
+  }
+
+  createContact($event: IContactItem): void {
+    this.contactsService.saveContact($event);
+  }
+
+  toggleFavorite($event: IContactItem): void {
+    this.contactsService.toggleFavorite($event);
   }
 }
