10b76ac5d5a538c87fd8c0a274a56cc67a47d019
Muhammad Umair <mumairofficial@gmail.com>
2019-08-11 11:35:44 +0500

    perf: shared module in action
    Exported commonly used modules to avoide duplicate imports in other modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,9 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-@NgModule({})
+@NgModule({
+  exports: [CommonModule, RouterModule, ReactiveFormsModule]
+})
 export class SharedModule {}
