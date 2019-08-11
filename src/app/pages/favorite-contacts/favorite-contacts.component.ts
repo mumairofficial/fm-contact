079ef8c487e2d8312a393f2cd0ea4afaaaba62f9
Muhammad Umair <mumairofficial@gmail.com>
2019-08-11 19:32:50 +0500

    feat: added logic for favorite contacts and filtered based upon favorite state

--- src/app/pages/favorite-contacts/favorite-contacts.component.ts
+++ src/app/pages/favorite-contacts/favorite-contacts.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IContactItem } from "src/app/shared/models/contact-item.interface";
+import { ContactService } from "src/app/core/contact.service";
 
 @Component({
   selector: "favorite-contacts-page",
   templateUrl: "favorite-contacts.component.html"
 })
-export class FavoriteContactsComponent {}
+export class FavoriteContactsComponent implements OnInit {
+  public favoriteContacts: Array<IContactItem> = [];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.favoriteContacts = this.contactService.contacts.filter(item => item.isFavorite);
+  }
+}
