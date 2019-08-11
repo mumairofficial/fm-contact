3733f872644c26769408c9c6648eff04b645bedf
Muhammad Umair <mumairofficial@gmail.com>
2019-08-11 19:32:03 +0500

    feat: dynamic contacts from global contact service logic

--- src/app/pages/contacts-home/contacts-home.component.ts
+++ src/app/pages/contacts-home/contacts-home.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IContactItem } from "src/app/shared/models/contact-item.interface";
+import { ContactService } from "src/app/core/contact.service";
 
 @Component({
   selector: "contacts-home-page",
   templateUrl: "contacts-home.component.html"
 })
-export class ContactsHomeComponent {}
+export class ContactsHomeComponent implements OnInit {
+  public contacts: Array<IContactItem> = [];
+
+  constructor(private contactsService: ContactService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactsService.contacts;
+  }
+}
