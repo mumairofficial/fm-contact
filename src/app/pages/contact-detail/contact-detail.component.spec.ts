a019b08a75f207b1b270fcc8a63d861979d235bf
Muhammad Umair <mumairofficial@gmail.com>
2019-08-12 08:12:14 +0500

    test: test updates

--- src/app/pages/contact-detail/contact-detail.component.spec.ts
+++ src/app/pages/contact-detail/contact-detail.component.spec.ts
@@ -1,18 +1,18 @@
 import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { SharedModule } from "src/app/shared";
-import { ContactsHomeComponent } from "./contacts-home.component";
+import { ContactDetailComponent } from "./contact-detail.component";
 
-describe("ContactHomePage", () => {
+describe("ContactDetailPage", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, SharedModule],
-      declarations: [ContactsHomeComponent]
+      declarations: [ContactDetailComponent]
     }).compileComponents();
   }));
 
-  it("should create the contact home page", () => {
-    const fixture = TestBed.createComponent(ContactsHomeComponent);
+  it("should create the contact detail page", () => {
+    const fixture = TestBed.createComponent(ContactDetailComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
