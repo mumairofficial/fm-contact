5a5e580ca887ae626e6a4b1ec9b0ee2111aaa6d6
Muhammad Umair <mumairofficial@gmail.com>
2019-08-12 09:48:58 +0500

    fix: default routing and path mis-match redirection fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,23 @@ import { LayoutComponent } from "./core/components";
 import { ContactsHomeComponent } from "./pages/contacts-home/contacts-home.component";
 
 const routes: Routes = [
+  // redirection default / to contacts page
+  { path: "", redirectTo: "contacts", pathMatch: "full" },
+
   {
     path: "",
     component: LayoutComponent,
     children: [
       // eager load contact home component while application loading
       {
-        path: "",
-        component: ContactsHomeComponent
+        path: "contacts",
+        component: ContactsHomeComponent,
+        pathMatch: "full"
+      },
+
+      {
+        path: "contacts/:id",
+        loadChildren: () => import("./pages/contact-detail/contact-detail.module").then(m => m.ContactDetailModule)
       },
 
       // lazy route to load chunk on demand rather including in main app chunk
@@ -24,7 +33,7 @@ const routes: Routes = [
   },
 
   // check if path does not match with any of above the redirect to hom
-  { path: "**", redirectTo: "/" }
+  { path: "**", redirectTo: "contacts", pathMatch: "full" }
 ];
 
 @NgModule({
