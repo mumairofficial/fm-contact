34b69df9bcae62c7f63a3141e3b23ff4d5fa1dd3
Muhammad Umair <mumairofficial@gmail.com>
2019-08-13 10:48:46 +0500

    feat: integrated detail and update page

--- src/app/pages/contact-detail/contact-detail.component.ts
+++ src/app/pages/contact-detail/contact-detail.component.ts
@@ -1,7 +1,28 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IContactItem } from "src/app/shared/models/contact-item.interface";
+import { ContactService } from "src/app/core/contact.service";
 
 @Component({
   selector: "contact-detail-page",
   templateUrl: "contact-detail.component.html"
 })
-export class ContactDetailComponent {}
+export class ContactDetailComponent {
+  public contact: IContactItem;
+
+  constructor(private route: ActivatedRoute, private router: Router, private contactService: ContactService) {
+    this.route.paramMap.subscribe(params => {
+      const contactId = params.get("id");
+      this.fetchContact(Number(contactId));
+    });
+  }
+
+  private fetchContact(contactId): void {
+    this.contact = this.contactService.getContact(contactId);
+  }
+
+  public updateContact(contact: IContactItem) {
+    this.contactService.updateContact(contact);
+    this.router.navigate(["/contacts"]);
+  }
+}
