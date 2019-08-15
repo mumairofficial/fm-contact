b870cbaaecdae86801122f8fb1f5118f2c0acc37
Muhammad Umair <mumairofficial@gmail.com>
2019-08-15 18:54:13 +0500

    feat: custom phone validator

--- src/app/shared/validators/custom.validators.ts
+++ src/app/shared/validators/custom.validators.ts
@@ -6,4 +6,12 @@ export class CustomValidators {
     const isValid = !isWhitespace;
     return isValid ? null : { whitespace: true };
   }
+
+  public static phone(control: FormControl) {
+    // https://stackoverflow.com/questions/123559/a-comprehensive-regex-for-phone-number-validation
+    const phoneNumber = (control.value || "").trim();
+    const regex = /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i;
+    const isValid = regex.test(phoneNumber)
+    return isValid ? null : { phone: true };
+  }
 }
