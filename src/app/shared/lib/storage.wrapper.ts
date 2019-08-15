331d2eed4945886f47edaaee1c75ce6d8dd057fa
Muhammad Umair <mumairofficial@gmail.com>
2019-08-15 13:52:22 +0500

    feat: local storage wrapper

---
+++ src/app/shared/lib/storage.wrapper.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IContactItem } from '../models/contact-item.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Storage {
+  public save(key: string, value: object): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  public get(key: string): Array<IContactItem> {
+    const values = localStorage.getItem(key);
+    if (values === null) {
+      return null;
+    }
+    return JSON.parse(localStorage.getItem(key));
+  }
+}
